779399ba8d851e5da38940cefed0c1de9e5dbc93
Mohammad-asimm8601 <mohammadasim84924@gmail.com>
2026-09-04 21:32:26 +0530

    matplotlib

---
+++ data visualization/matplotlib_demo.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+for i in range(2):
+    plt.plot(np.random.rand(100), linewidth=1)
+
+plt.title("Too much data can be confusing!")
+plt.grid(True)
+plt.tight_layout()
+plt.show()
